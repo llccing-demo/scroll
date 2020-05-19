639c77e2218c7dc74bbb20931dc29b3a28edaa70
liuchunfeng <liuchunfeng@didiglobal.com>
2020-05-19 11:52:01 +0800

    update 更新 tiger 效果

--- README.md
+++ README.md
@@ -6,4 +6,27 @@
 
 ### 使用 better-scroll 实现 swiper 的 example
 
-* [ ] fresh -- 每日优鲜效果
+* [x] fresh -- 每日优鲜
+* [x] taobao-world -- 淘宝地球
+* [ ] tiger -- 打虎幻灯片
+
+#### fresh
+
+这个页面的布局很复杂，涉及到多层嵌套，处理的时候需要注意。
+
+
+#### taobao-world
+
+这个 slide 两边留白的效果在很多的APP中都出现过，是个很有用的效果。并且这个效果能够扩展为其他很多效果，可以尝试。
+
+地球的滚动的效果主要是监听 slide 滚动的距离，计算百分比，这个可以考虑放到 slide 的组件内部做为事件的回调参数来使用会更加方便。
+
+
+#### tiger
+
+首先使用了 animate.css 类库实现效果。
+
+**有个默认展示的问题**，解决方式如下，类似幻灯片的效果实现，一个特别有用的属性是 `animation-fill-mode: none | forwards | backwards | both`， none 表示不改变默认行为；forwards 表示当动画执行完成后，保持最后一个属性值（在最后一个关键帧中定义）；backwards 表示在 animation-delay 所指定的一段时间内，在动画显示之前，应用开始属性值（在第一个关键帧中定义）；both 表示向前和向后填充模式都被应用。
+
+- [https://segmentfault.com/q/1010000003867335](https://segmentfault.com/q/1010000003867335) 如何理解 animation-fill-mode 使用
+- [https://developer.mozilla.org/zh-CN/docs/Web/CSS/animation-fill-mode](https://developer.mozilla.org/zh-CN/docs/Web/CSS/animation-fill-mode) MDN animation-fill-mode
--- packages/bscroll/src/views/tiger/index.vue
+++ packages/bscroll/src/views/tiger/index.vue
@@ -32,10 +32,68 @@
           alt="text"
         />
       </cube-slide-item>
-      <cube-slide-item class="slide2"></cube-slide-item>
-      <cube-slide-item class="slide3"></cube-slide-item>
-      <cube-slide-item class="slide4"></cube-slide-item>
+      <cube-slide-item class="slide1">
+        <img
+          ref="slide1_title"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/004.png"
+          alt="title"
+          style="margin-top: 50px"
+        />
+        <img
+          ref="slide1_sub_title"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/005.png"
+          alt="sub-title"
+          style="margin-top: 50px"
+        />
+        <img
+          ref="slide1_content"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/006.png"
+          alt="content"
+          style="margin-top: 50px"
+        />
+      </cube-slide-item>
+      <cube-slide-item class="slide2">
+        <img
+          ref="slide2_title"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/007.png"
+          alt="title"
+          style="margin-top: 50px"
+        />
+        <img
+          ref="slide2_content1"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/009.png"
+          alt="content1"
+          style="margin-top: 50px"
+        />
+        <img
+          ref="slide2_content"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/008.png"
+          alt="content"
+          style="margin-top: 50px"
+        />
+        <img
+          class="content2"
+          ref="slide2_content2"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/010.png"
+          alt="content2"
+        />
+      </cube-slide-item>
+      <cube-slide-item class="slide3">
+        <img
+          ref="slide3_title"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/001.jpg"
+          alt="title"
+          style="margin-top: 50px"
+        />
+        <img
+          ref="slide3_content"
+          src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/002.jpg"
+          alt="content"
+          style="margin-top: 50px"
+        />
+      </cube-slide-item>
     </cube-slide>
+    <img class="arrow" src="http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/images/arrow.png" alt="arrow">
   </div>
 </template>
 <script>
@@ -64,6 +122,58 @@ export default {
         slide0_text.classList.remove('fadeInUp')
       }
     },
+
+    slide1Animation(isShow = true) {
+      const {
+        slide1_title,
+        slide1_sub_title,
+        slide1_content
+      } = this.$refs
+      if (isShow) {
+        slide1_title.classList.add('bounce')
+        slide1_sub_title.classList.add('fadeIn')
+        slide1_content.classList.add('zoomInDown')
+      } else {
+        slide1_title.classList.remove('bounce')
+        slide1_sub_title.classList.remove('fadeIn')
+        slide1_content.classList.remove('zoomInDown')
+      }
+    },
+
+    slide2Animation(isShow = true) {
+      const {
+        slide2_title,
+        slide2_content1,
+        slide2_content,
+        slide2_content2
+      } = this.$refs
+      if (isShow) {
+        slide2_title.classList.add('bounce')
+        slide2_content1.classList.add('zoomIn')
+        slide2_content.classList.add('bounceInLeft')
+        slide2_content2.classList.add('bounceInRight')
+      } else {
+        slide2_title.classList.remove('bounce')
+        slide2_content1.classList.remove('zoomIn')
+        slide2_content.classList.remove('bounceInLeft')
+        slide2_content2.classList.remove('bounceInRight')
+      }
+    },
+
+    slide3Animation(isShow = true) {
+      const {
+        slide3_title,
+        slide3_content
+      } = this.$refs
+      if (isShow) {
+        slide3_title.classList.add('fadeInDown')
+        slide3_content.classList.add('zoomInLeft')
+      } else {
+        slide3_title.classList.remove('fadeInDown')
+        slide3_content.classList.remove('zoomInLeft')
+      }
+    },
+
     onChange(index) {
       this[`slide${index}Animation`] && this[`slide${index}Animation`]()
       setTimeout(() => {
@@ -72,22 +182,18 @@ export default {
     }
   },
   mounted() {
+    // this._toast = this.$createToast(Object.assign({
+    //   type: 'warn',
+    //   txt: '已存在运输证，请勿重复提交已存在运输证，请勿重复提交已存在运输证，请勿重复提交已存在运输证，请勿重复提交已存在运输证，请勿重复提交',
+    //   time: 2000
+    // }, {}))
+    // this._toast.show()
     this.onChange(0)
   }
 }
 </script>
 <style lang="stylus">
 .tiger
-  .zoomIn
-    animation zoomIn 1.5s 0s
-  .fadeIn
-    animation fadeIn 0.5s 1.5s
-  .rotateInDownLeft
-    animation rotateInDownLeft 0.5s 2s
-  .bounceInDown
-    animation bounceInDown 1.5s 2.5s
-  .fadeInUp
-    animation fadeInUp 0.5s 4s
   img
     display block
     width 100%
@@ -96,9 +202,44 @@ export default {
     background url('http://static.llccing.cn/llccing-demo/scroll/swiper/tiger/upload/bg.jpg') no-repeat left top
   .slide0
     position relative
+    .zoomIn
+      animation zoomIn 1.5s 0s both
+    .fadeIn
+      animation fadeIn 0.5s 1.5s both
+    .rotateInDownLeft
+      animation rotateInDownLeft 0.5s 2s both
+    .bounceInDown
+      animation bounceInDown 1.5s 2.5s both
+    .fadeInUp
+      animation fadeInUp 0.5s 4s both
     .tiger
       position absolute
       top 193px
+  .slide1
+    .bounce
+      animation bounce 0.5s 0s both
+    .fadeIn
+      animation fadeIn 0.5s 0.5s both
+    .zoomInDown
+      animation zoomInDown 0.5s 1s both
+  .slide2
+    position relative
+    .content2
+      position absolute
+      top 340px
+    .bounce
+      animation bounce 0.5s 0s both
+    .zoomIn
+      animation zoomIn 0.5s 0.5s both
+    .bounceInLeft
+      animation bounceInLeft 0.5s 1s both
+    .bounceInRight
+      animation bounceInRight 0.5s 1.5s both
+  .slide3
+    .fadeInDown
+      animation fadeInDown 0.5s 0s both
+    .zoomInLeft
+      animation zoomInLeft 0.5s 0.5s both
   .cube-slide
     .cube-slide-dots
       left initial
@@ -115,4 +256,23 @@ export default {
         margin 5px 0
         &.active
           // background #fff
+  .arrow
+    position fixed
+    bottom 20px
+    left 0
+    right 0
+    margin 0 auto
+    width 20px
+    height 20px
+    animation start 1.5s infinite
+  @keyframes start 
+    0%, 30%
+      opacity 0
+      transform translate(0, 10px)
+    60%
+      opacity 1
+      transform translate(0, 0)
+    100%
+      opacity 0
+      transform translate(0, -8px)
 </style>
